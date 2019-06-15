eed7cb7fdafe6b3f9cc937a7d8c1b97e4c2930dd
Aditya Keyal <adi.keyal@gmail.com>
2019-06-15 22:38:52 +0530

    Updated

---
+++ mail.py
@@ -0,0 +1,139 @@
+
+
+import smtplib
+import time
+import imaplib
+import email
+import os
+from datetime import datetime,timedelta
+
+#
+# ORG_EMAIL   = "@<>.com"
+# FROM_EMAIL  = "" + ORG_EMAIL
+# FROM_PWD    = os.environ['USER_PASSWORD']
+# SMTP_SERVER = "imap.gmail.com"
+# SMTP_PORT   = 993
+
+class ImapWrapper:
+
+    mail = None
+    utility = None
+
+    def __init__(self,smtp_server):
+        self.mail = imaplib.IMAP4_SSL(smtp_server)
+        self.utility = Utility()
+        pass
+
+    def login(self,username:str , password : str) :
+        self.mail.login(username, password)
+        pass
+
+    def list_mail_boxes(self):
+        mailbox = self.mail.list()
+        boxes = []
+        for item in mailbox[1]:
+            flags , box = self.utility.extract_mail_box(item.decode())
+            if "\\Noselect" not in flags:
+                boxes.append(box)
+        return boxes
+
+    def select_mail_box(self,inbox_name):
+        self.mail.select(inbox_name)
+
+    def fetch_mail(self,uid):
+        result, data = self.mail.fetch(uid,'(RFC822)')
+        return self.utility.extract_body(data)
+        pass
+
+    def search(self, unread = True , since = (datetime.now() - timedelta(days=180)), before = None , sender = None):
+        args = []
+
+        if unread:
+            args.append('UNSEEN')
+        if since is not None:
+            args.append('SINCE')
+            args.append(since.strftime('%d-%b-%Y'))
+        if before is not None:
+            args.append('BEFORE')
+            args.append(before.strftime('%d-%b-%Y'))
+        if sender is not None:
+            args.append('FROM')
+            args.append(sender)
+
+        result , data = self.mail.search(None,*args)
+        if result == 'OK':
+            return data[0].decode().split()
+        return []
+
+    def __del__(self):
+        if self.mail is not None:
+            try:
+                self.mail.close()
+            except:
+                pass
+        pass
+
+
+
+class Utility:
+
+    def extract_body(self,data):
+        response = {'attachments':[]};
+        for response_part in data:
+            if isinstance(response_part, tuple):
+                msg = email.message_from_bytes((response_part[1]))
+                for m in msg.walk():
+                    email_subject = m['subject']
+                    email_from = m['from']
+                    content = m.get_payload(decode=True)
+                    content_type = m['Content-Type']
+
+                    if email_from is not None:
+                        response['from']=email_from
+                    if email_subject is not None:
+                        response['subject']=email_subject
+                    if content is not None:
+                        if m.get_content_type() != 'application/octet-stream':
+                           response['text']=content.decode("utf-8")
+                        else:
+                            f = open(m.get_filename(),'wb')
+                            f.write(content)
+                            f.close()
+                            response['attachments'].append(m.get_filename())
+        return response
+
+    def extract_mail_box(self,mail_box_response):
+        # Format : flags, the hierarchy delimiter, and mailbox name
+        flags = ""
+        delimiter = None
+        mailbox_name = ""
+        _flag_start = False
+
+        for ch in mail_box_response:
+            if ch == '(':
+                _flag_start = True
+                continue
+            if ch == ')':
+                _flag_start= False
+                continue
+            if ch == '\"':
+                continue
+            if _flag_start:
+                flags+=ch
+                continue
+            if delimiter is None and ch != ' ':
+                delimiter = ch
+                pass
+            else:
+                mailbox_name+=ch
+            pass
+        return flags,'"'+mailbox_name.strip()+'"'
+
+if __name__ == '__main__':
+    m = ImapWrapper("imap.gmail.com")
+    m.login("","")
+    m.select_mail_box('"Inbox"')
+    ids = m.search(unread=False)
+    for x in ids:
+        content = m.fetch_mail(x)
+        print(content)
